88e906dcd9f4e82f2b80077206df3051b49284d4
noahihi203 <da02470247@gmail.com>
2025-05-05 18:44:52 +0700

    feat: Implement advanced property filtering and search functionality
    - Added FiltersBar and FiltersFull components for user-friendly filtering options.
    - Integrated Redux state management for filters, view mode, and filter visibility.
    - Enhanced property fetching with new filters in the API.
    - Created Listings and Map components to display properties based on selected filters.
    - Improved UI components (Card and CardCompact) for better property representation.
    - Added amenities and available date filters to enhance search capabilities.
    - Implemented debounce for location search to optimize performance.

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,7 +1,8 @@
-import { createNewUserInDatabase } from "@/lib/utils";
-import { Manager, Tenant } from "@/types/prismaTypes";
+import { cleanParams, createNewUserInDatabase, withToast } from "@/lib/utils";
+import { Manager, Property, Tenant } from "@/types/prismaTypes";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { fetchAuthSession, getCurrentUser } from "aws-amplify/auth";
+import { FiltersState } from ".";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
@@ -15,7 +16,7 @@ export const api = createApi({
     },
   }),
   reducerPath: "api",
-  tagTypes: ["Managers", "Tenants"],
+  tagTypes: ["Managers", "Tenants", "Properties"],
   endpoints: (build) => ({
     getAuthUser: build.query<User, void>({
       queryFn: async (_, _queryApi, _extraOptions, fetchWithBQ) => {
@@ -54,6 +55,20 @@ export const api = createApi({
         }
       },
     }),
+
+    updateManagerSettings: build.mutation<
+      Manager,
+      { cognitoId: string } & Partial<Manager>
+    >({
+      query: ({ cognitoId, ...updatedManager }) => ({
+        url: `managers/${cognitoId}`,
+        method: "PUT",
+        body: updatedManager,
+      }),
+      invalidatesTags: (result) => [{ type: "Managers", id: result?.id }],
+    }),
+
+    //property related endpoints
     updateTenantSettings: build.mutation<
       Tenant,
       { cognitoId: string } & Partial<Tenant>
@@ -67,16 +82,75 @@ export const api = createApi({
       invalidatesTags: (result) => [{ type: "Tenants", id: result?.id }],
     }),
 
-    updateManagerSettings: build.mutation<
-      Manager,
-      { cognitoId: string } & Partial<Manager>
+    getProperties: build.query<
+      Property[],
+      Partial<FiltersState> & { favoriteIds?: number[] }
     >({
-      query: ({ cognitoId, ...updatedManager }) => ({
-        url: `managers/${cognitoId}`,
-        method: "PUT",
-        body: updatedManager,
+      query: (filters) => {
+        const params = cleanParams({
+          location: filters.location,
+          priceMin: filters.priceRange?.[0],
+          priceMax: filters.priceRange?.[1],
+          beds: filters.beds,
+          baths: filters.baths,
+          propertyType: filters.propertyType,
+          squareFeetMin: filters.squareFeet?.[0],
+          squareFeetMax: filters.squareFeet?.[1],
+          amenities: filters.amenities?.join(","),
+          availableFrom: filters.availableFrom,
+          favoriteIds: filters.favoriteIds?.join(","),
+          latitude: filters.coordinates?.[1],
+          longitude: filters.coordinates?.[0],
+        });
+
+        return { url: "properties", params };
+      },
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: "Properties" as const, id })),
+              { type: "Properties", id: "LIST" },
+            ]
+          : [{ type: "Properties", id: "LIST" }],
+    }),
+    // tenant related endpoints
+    getTenant: build.query<Tenant, string>({
+      query: (cognitoId) => `tenants/${cognitoId}`,
+      providesTags: (result) => [{ type: "Tenants", id: result?.id }],
+    }),
+
+    addFavoriteProperty: build.mutation<
+      Tenant,
+      { cognitoId: string; propertyId: number }
+    >({
+      query: ({ cognitoId, propertyId }) => ({
+        url: `tenants/${cognitoId}/favorites/${propertyId}`,
+        method: "POST",
       }),
-      invalidatesTags: (result) => [{ type: "Managers", id: result?.id }],
+      invalidatesTags: (result) => [
+        {
+          type: "Tenants",
+          id: result?.id,
+        },
+        { type: "Properties", id: "LIST" },
+      ],
+    }),
+
+    removeFavoriteProperty: build.mutation<
+      Tenant,
+      { cognitoId: string; propertyId: number }
+    >({
+      query: ({ cognitoId, propertyId }) => ({
+        url: `tenants/${cognitoId}/favorites/${propertyId}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: (result) => [
+        {
+          type: "Tenants",
+          id: result?.id,
+        },
+        { type: "Properties", id: "LIST" },
+      ],
     }),
   }),
 });
@@ -85,4 +159,8 @@ export const {
   useGetAuthUserQuery,
   useUpdateTenantSettingsMutation,
   useUpdateManagerSettingsMutation,
+  useGetPropertiesQuery,
+  useGetTenantQuery,
+  useAddFavoritePropertyMutation,
+  useRemoveFavoritePropertyMutation,
 } = api;
--- server/src/controllers/propertyControllers.ts
+++ server/src/controllers/propertyControllers.ts
@@ -2,14 +2,16 @@ import { Request, Response } from "express";
 import { PrismaClient, Prisma } from "@prisma/client";
 import { wktToGeoJSON } from "@terraformer/wkt";
 import { S3Client } from "@aws-sdk/client-s3";
-import { Upload } from "@aws-sdk/lib-storage";
 import { Location } from "@prisma/client";
+import { Upload } from "@aws-sdk/lib-storage";
 import axios from "axios";
 
 const prisma = new PrismaClient();
+
 const s3Client = new S3Client({
   region: process.env.AWS_REGION,
 });
+
 export const getProperties = async (
   req: Request,
   res: Response
@@ -29,6 +31,7 @@ export const getProperties = async (
       latitude,
       longitude,
     } = req.query;
+
     let whereConditions: Prisma.Sql[] = [];
 
     if (favoriteIds) {
@@ -37,11 +40,13 @@ export const getProperties = async (
         Prisma.sql`p.id IN (${Prisma.join(favoriteIdsArray)})`
       );
     }
+
     if (priceMin) {
       whereConditions.push(
         Prisma.sql`p."pricePerMonth" >= ${Number(priceMin)}`
       );
     }
+
     if (priceMax) {
       whereConditions.push(
         Prisma.sql`p."pricePerMonth" <= ${Number(priceMax)}`
@@ -88,7 +93,7 @@ export const getProperties = async (
           whereConditions.push(
             Prisma.sql`EXISTS (
               SELECT 1 FROM "Lease" l 
-              WHERE l."propertyId" = p.id
+              WHERE l."propertyId" = p.id 
               AND l."startDate" <= ${date.toISOString()}
             )`
           );
@@ -100,7 +105,7 @@ export const getProperties = async (
       const lat = parseFloat(latitude as string);
       const lng = parseFloat(longitude as string);
       const radiusInKilometers = 1000;
-      const degrees = radiusInKilometers / 111;
+      const degrees = radiusInKilometers / 111; // Converts kilometers to degrees
 
       whereConditions.push(
         Prisma.sql`ST_DWithin(
@@ -122,16 +127,17 @@ export const getProperties = async (
           'country', l.country,
           'postalCode', l."postalCode",
           'coordinates', json_build_object(
-            'latitude', ST_X(l."coordinates::geometry),"),
-            'longitude', ST_Y(l."coordinates::geometry)")
-          )) as location,
-          FROM "Property" p
-          JOIN "Location" l ON p."locationId" = l.id
-          ${
-            whereConditions.length > 0
-              ? Prisma.sql`WHERE ${Prisma.join(whereConditions, " AND ")}`
-              : Prisma.empty
-          }
+            'longitude', ST_X(l."coordinates"::geometry),
+            'latitude', ST_Y(l."coordinates"::geometry)
+          )
+        ) as location
+      FROM "Property" p
+      JOIN "Location" l ON p."locationId" = l.id
+      ${
+        whereConditions.length > 0
+          ? Prisma.sql`WHERE ${Prisma.join(whereConditions, " AND ")}`
+          : Prisma.empty
+      }
     `;
 
     const properties = await prisma.$queryRaw(completeQuery);
@@ -159,11 +165,11 @@ export const getProperty = async (
 
     if (property) {
       const coordinates: { coordinates: string }[] =
-        await prisma.$queryRaw`SELECT ST_asText(coordinates) as coordinates FROM "Location" WHERE id = ${property.location.id}`;
+        await prisma.$queryRaw`SELECT ST_asText(coordinates) as coordinates from "Location" where id = ${property.location.id}`;
 
-      const geoJson: any = wktToGeoJSON(coordinates[0]?.coordinates || "");
-      const longitude = geoJson.coordinates[0];
-      const latitude = geoJson.coordinates[1];
+      const geoJSON: any = wktToGeoJSON(coordinates[0]?.coordinates || "");
+      const longitude = geoJSON.coordinates[0];
+      const latitude = geoJSON.coordinates[1];
 
       const propertyWithCoordinates = {
         ...property,
@@ -177,10 +183,10 @@ export const getProperty = async (
       };
       res.json(propertyWithCoordinates);
     }
-  } catch (error: any) {
+  } catch (err: any) {
     res
       .status(500)
-      .json({ message: `Error retrieving property: ${error.message}` });
+      .json({ message: `Error retrieving property: ${err.message}` });
   }
 };
 
@@ -213,9 +219,11 @@ export const createProperty = async (
           client: s3Client,
           params: uploadParams,
         }).done();
+
         return uploadResult.Location;
       })
     );
+
     const geocodingUrl = `https://nominatim.openstreetmap.org/search?${new URLSearchParams(
       {
         street: address,
@@ -228,7 +236,7 @@ export const createProperty = async (
     ).toString()}`;
     const geocodingResponse = await axios.get(geocodingUrl, {
       headers: {
-        "User-Agent": "RealEstateApp (justsomedummyemail@gmail.com)",
+        "User-Agent": "RealEstateApp (justsomedummyemail@gmail.com",
       },
     });
     const [longitude, latitude] =
@@ -239,14 +247,14 @@ export const createProperty = async (
           ]
         : [0, 0];
 
-    //create location
+    // create location
     const [location] = await prisma.$queryRaw<Location[]>`
       INSERT INTO "Location" (address, city, state, country, "postalCode", coordinates)
       VALUES (${address}, ${city}, ${state}, ${country}, ${postalCode}, ST_SetSRID(ST_MakePoint(${longitude}, ${latitude}), 4326))
       RETURNING id, address, city, state, country, "postalCode", ST_AsText(coordinates) as coordinates;
     `;
 
-    //create property
+    // create property
     const newProperty = await prisma.property.create({
       data: {
         ...propertyData,
@@ -273,12 +281,13 @@ export const createProperty = async (
       include: {
         location: true,
         manager: true,
-      }
+      },
     });
-    res.status(201).json(newProperty)
-  } catch (error: any) {
+
+    res.status(201).json(newProperty);
+  } catch (err: any) {
     res
       .status(500)
-      .json({ message: `Error creating property: ${error.message}` });
+      .json({ message: `Error creating property: ${err.message}` });
   }
 };
