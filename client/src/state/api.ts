67d496812b235526d68775169fbafb8983e8d2e7
noahihi203 <da02470247@gmail.com>
2025-05-08 00:27:31 +0700

    feat: Implement NewProperty component for adding new property listings with detailed information

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -188,6 +188,21 @@ export const api = createApi({
           : [{ type: "Properties", id: "LIST" }],
     }),
 
+    createProperty: build.mutation<Property, FormData>({
+      query: (newProperty) => ({
+        url: `properties`,
+        method: "POST",
+        body: newProperty,
+      }),
+      invalidatesTags: (result) => [
+        { type: "Properties", id: "LIST" },
+        {
+          type: "Managers",
+          id: result?.manager?.id,
+        },
+      ],
+    }),
+
     //lease related endpoint
     getLeases: build.query<Lease[], number>({
       query: () => "leases",
@@ -214,10 +229,11 @@ export const {
   useGetPropertyQuery,
   useGetCurrentResidencesQuery,
   useGetManagerPropertiesQuery,
+  useCreatePropertyMutation,
   useGetTenantQuery,
   useAddFavoritePropertyMutation,
   useRemoveFavoritePropertyMutation,
   useGetLeasesQuery,
   useGetPropertyLeasesQuery,
-  useGetPaymentsQuery
+  useGetPaymentsQuery,
 } = api;
