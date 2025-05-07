9ba7c48f087e876594be32b42f5b1e56582d4247
noahihi203 <da02470247@gmail.com>
2025-05-07 18:23:31 +0700

    feat: Implement Favorites page with property listing and loading/error handling

--- client/src/app/(dashboard)/tenants/favorites/page.tsx
+++ client/src/app/(dashboard)/tenants/favorites/page.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
+"use client";
+
+import Card from "@/components/Card";
+import Header from "@/components/Header";
+import Loading from "@/components/Loading";
+import {
+  useGetAuthUserQuery,
+  useGetPropertiesQuery,
+  useGetTenantQuery,
+} from "@/state/api";
+import React from "react";
 
 const Favorites = () => {
-  return (
-    <div>Favorites</div>
-  )
-}
+  const { data: authUser } = useGetAuthUserQuery();
+  const { data: tenant } = useGetTenantQuery(
+    authUser?.cognitoInfo?.userId || "",
+    {
+      skip: !authUser?.cognitoInfo?.userId,
+    }
+  );
+  const {
+    data: favoritesProperties,
+    isLoading,
+    error,
+  } = useGetPropertiesQuery(
+    { favoriteIds: tenant?.favorites.map((fav: { id: number }) => fav.id) },
+    { skip: tenant?.favorites.length === 0 || !tenant?.favorites }
+  );
 
-export default Favorites
+  if (isLoading) return <Loading />;
+  if (error) return <div>Error loading favorites</div>;
+
+  return (
+    <div className="dashboard-container">
+      <Header
+        title="Favorited Properties"
+        subtitle="Browse and manage your saved property listings"
+      />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        {favoritesProperties?.map((property) => (
+          <Card
+            key={property.id}
+            property={property}
+            isFavorite={true}
+            onFavoriteToggle={() => {}}
+            showFavoriteButton={false}
+            propertyLink={`/tenants/residences/${property.id}`}
+          />
+        ))}
+      </div>
+      {(!favoritesProperties || favoritesProperties.length === 0) && (
+        <p>You don&pos;t have any fovorited properties</p>
+      )}
+    </div>
+  );
+};
+
+export default Favorites;
