7e429faa7a4c656810fbe3f460912f2410c98ad7
noahihi203 <da02470247@gmail.com>
2025-05-08 14:25:44 +0700

    feat: Update ESLint configuration and add ecosystem config for project management

--- server/src/index.ts
+++ server/src/index.ts
@@ -39,7 +39,7 @@ app.use("/managers", authMiddleware(["manager"]), managerRoutes);
 
 /* SERVER */
 
-const port = process.env.PORT || 3002;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3002;
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port} `);
 });
